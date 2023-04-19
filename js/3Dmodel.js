1778b994c00b8ac52cda9e6e35c6085a17d5f7fe
Liseth Mendoza <lismc1992@gmail.com>
2023-04-19 20:35:18 +0200

    Add more light to model

--- js/3Dmodel.js
+++ js/3Dmodel.js
@@ -5,14 +5,18 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 //*  Scene and render 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x524961);
 const camera = new THREE.PerspectiveCamera(
 30,
 window.innerWidth / window.innerHeight
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x0e2f44);
+//renderer.setClearColor(0x0e2f44);
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 6;
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 
@@ -21,16 +25,11 @@ const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(30, 10, 10);
 controls.update();
 
-scene.add(new THREE.AxesHelper(10));
+//scene.add(new THREE.AxesHelper(10));
 
 //* Add light
-const light = new THREE.AmbientLight(0xffffff, 0.5);
+const light = new THREE.AmbientLight(0xffffff, 100);
 scene.add(light);
-
-const dl = new THREE.DirectionalLight(0xffffff, 0,5);
-dl.position.set(0, 6, 0);
-const dlHelper = new THREE.DirectionalLightHelper(dl, 3);
-scene.add(dl, dlHelper);
 
 
 / Add 3D model */
@@ -40,6 +39,7 @@ gltfLoader.load('/assets/models/scene.gltf', (gltf) => {
     gltf.scene.position.y = -3.5; //position model top/bottom
     gltf.scene.scale.set(10, 10, 10);
     scene.add(gltf.scene);
+
 });
 
 function animate() {
